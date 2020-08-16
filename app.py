d5ebc48120e8687015a84da538d01d2b8b91f7d7
abc <abc@example.com>
2020-08-16 19:55:02 +0500

    Code Updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template,request,send_from_directory,redirect,url_for,Request,Response
 import videoPreprocessing
 from werkzeug.utils import secure_filename
-import os,re,mimetypes
+import os,re,time
 
 
 app = Flask(__name__)
@@ -36,8 +36,15 @@ def upload_file():
       f = request.files['myfile']
       path=os.path.join(app.config['UPLOAD_FOLDER'], "sample.mp4")
       f.save(path)
+      for filename in os.listdir('static/'):
+        if filename.startswith('projectwithaudio'):  # not to remove other files
+            os.remove('static/' + filename)
       videoPreprocessing.video()
-      return render_template('display.html')
+      
+      new_file_name = "projectwithaudio" + str(time.time()) + ".mp4"
+      os.rename(os.path.join(app.config['UPLOAD_FOLDER'], "projectwithaudio.mp4"),
+                os.path.join(app.config['UPLOAD_FOLDER'], new_file_name))
+      return render_template('display.html',filename=new_file_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
